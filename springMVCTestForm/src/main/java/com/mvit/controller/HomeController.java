9906b6398c5a9a6c7b1cfc1a400944c928eeddf4
Victor E. Márquez Flores <victor.marquez.flores@gmail.com>
2020-06-04 12:36:42 -0500

    cambio de HomeController.java vemf)

--- springMVCTestForm/src/main/java/com/mvit/controller/HomeController.java
+++ springMVCTestForm/src/main/java/com/mvit/controller/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Esta es la página  home por omisión {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
